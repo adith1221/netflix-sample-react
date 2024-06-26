660d9d20522c22f209f42a92160babc9e2d48186
adith m <adithsaseendran@gmail.com>
2024-06-26 21:14:51 +0530

    deleted unwanted and added som sample code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
+  const data='adith'
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='people'>hello</h1>
+      <p> this is {data} </p>
+      <br></br>
+      <Hello/>
     </div>
+    
   );
 }
 
 export default App;
+
+function Hello()
+{
+  return(
+    <h1 className='hello'>hello this is another text</h1>
+  )
+}
